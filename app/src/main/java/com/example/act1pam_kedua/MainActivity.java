cb71a6aaa8eab7517daa4725a859f198c37c0cc8
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2022-03-02 00:15:00 +0700

    Initial linear_layout

---
+++ app/src/main/java/com/example/act1pam_kedua/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.act1pam_kedua;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
